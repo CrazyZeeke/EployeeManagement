b5eef05054eb670172e7e5192d45ed8c198f18a6
Lovesmiles2 <joe9056@nyp.org>
2020-08-15 15:20:14 -0400

    First Commit

--- Startup.cs
+++ Startup.cs
@@ -32,7 +32,7 @@ namespace EployeeManagement
             {
                 endpoints.MapGet("/", async context =>
                 {
-                    await context.Response.WriteAsync("Hello World!");
+                    await context.Response.WriteAsync("Hello Johnny!");
                 });
             });
         }
